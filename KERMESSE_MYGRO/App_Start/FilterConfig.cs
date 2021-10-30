afa9f0aa7df07be90bd12d7eaa3d94e835f1cb01
xflare10 <xflare10@gmail.com>
2021-10-30 15:34:45 -0600

    Add project files.

---
+++ KERMESSE_MYGRO/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace KERMESSE_MYGRO
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ KERMESSE_MYGRO/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KERMESSE_MYGRO.MvcApplication" Language="C#" %>
