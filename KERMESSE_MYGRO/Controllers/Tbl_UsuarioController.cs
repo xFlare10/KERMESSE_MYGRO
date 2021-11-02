f686ff0608ced798c2568489243579182b8e0900
xflare10 <xflare10@gmail.com>
2021-11-01 18:31:04 -0600

    Interface added + User section // CRUD no functional yet

---
+++ KERMESSE_MYGRO/Controllers/Tbl_UsuarioController.cs
@@ -0,0 +1,19 @@
+﻿using KERMESSE_MYGRO.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KERMESSE_MYGRO.Controllers
+{
+    public class Tbl_UsuarioController : Controller
+    {
+        private KERMESSEEntities db = new KERMESSEEntities();
+        // GET: Tbl_Usuario
+        public ActionResult Tbl_Usuario()
+        {
+            return View(db.tbl_usuario.ToList());
+        }
+    }
+}
